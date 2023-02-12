60599755e82f3fa18346e5426d82a25243427004
BondarenkoTet <111590245+BondarenkoTet@users.noreply.github.com>
2023-02-12 20:08:09 +0000

    Deploying to gh-pages from @ BondarenkoTet/parcel-goit-js-hw-009@0d748fb06f8a82e48a19877b85b9736a1003448b 🚀

---
+++ 01-color-switcher.a4fbb809.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=0;e.addEventListener("click",(function(){a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!1,e.disabled=!0})),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.a4fbb809.js.map
